6fc55967cb22d48ff8d7c94c0adc44327b4d3e17
kirjavascript <snkenjoi@gmail.com>
2018-02-20 19:41:47 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// gtk / canvas
+// slowly get faster?
+
+extern crate gtk;
+extern crate gdk;
+extern crate gdk_pixbuf;
+
+fn main() {
+    println!("Hello, world!");
+
+
+    // let tick = move || {
+    //     gtk::Continue(true)
+    // };
+    // gtk::idle_add(tick);
+    gtk::main();
+}
