f8803158d16014bb4f8b3e6c21fe24996928c510
kirjavascript <snkenjoi@gmail.com>
2018-02-25 01:04:01 +0000

    controls

--- src/web.rs
+++ src/web.rs
@@ -4,6 +4,8 @@ extern crate stdweb;
 use stdweb::Value;
 use stdweb::web::*;
 use stdweb::web::html_element::CanvasElement;
+use stdweb::web::event::KeyDownEvent;
+use stdweb::traits::*;
 use stdweb::unstable::TryInto;
 
 use std::cell::RefCell;
@@ -27,17 +29,39 @@ fn main() {
     // TODO: snake.kirjava.xyz
     // TODO: https://rustbyexample.com/attribute/cfg.html
     // https://github.com/koute/pinky/blob/master/pinky-web/src/main.rs#L478
+    //You can, of course, use the js! macro too if you want. (Just remember that any callback which you pass through the js! macro needs to be manually dropped by calling .drop() when it's not used anymore;
 
+    // load snake, canvas
     let canvas: CanvasElement = document().query_selector("canvas").unwrap().unwrap().try_into().unwrap();
     let ctx: CanvasRenderingContext2d = canvas.get_context().unwrap();
-
     let snake = Rc::new(RefCell::new(Snake::new(64, 48)));
-    async_render_loop(snake, ctx);
+
+    // input
+    let snake_input_clone = snake.clone();
+    document().add_event_listener(move |e: KeyDownEvent| {
+        if snake_input_clone.borrow().is_running() {
+            let change = match e.key().as_str() {
+                "ArrowUp" => Some(snake::Direction::Up),
+                "ArrowDown" => Some(snake::Direction::Down),
+                "ArrowRight" => Some(snake::Direction::Right),
+                "ArrowLeft" => Some(snake::Direction::Left),
+                _ => None,
+            };
+            if change.is_some() {
+                snake_input_clone.borrow_mut().change_direction(change.unwrap());
+            }
+        }
+    });
+
+    // TODO: is clone needed?
+    async_render_loop(snake.clone(), ctx);
+
     stdweb::event_loop();
 }
+// pub fn set_timeout<F: FnOnce() + 'static>(callback: F, timeout: u32)
 
 fn async_render_loop(snake: Rc<RefCell<Snake>>, ctx: CanvasRenderingContext2d) {
-    window().request_animation_frame( move |_| {
+    window().request_animation_frame(move |timestamp| {
         snake.borrow_mut().step();
 
         let board: Value = snake.borrow_mut().get_rgba().into();
