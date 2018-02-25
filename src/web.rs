b1fc937c62fcec1024a136d571e7e685abc46af8
kirjavascript <snkenjoi@gmail.com>
2018-02-25 01:22:17 +0000

    main snake game implementation

--- src/web.rs
+++ src/web.rs
@@ -28,13 +28,14 @@ fn main() {
 
     // TODO: snake.kirjava.xyz
     // TODO: https://rustbyexample.com/attribute/cfg.html
-    // https://github.com/koute/pinky/blob/master/pinky-web/src/main.rs#L478
-    //You can, of course, use the js! macro too if you want. (Just remember that any callback which you pass through the js! macro needs to be manually dropped by calling .drop() when it's not used anymore;
 
     // load snake, canvas
     let canvas: CanvasElement = document().query_selector("canvas").unwrap().unwrap().try_into().unwrap();
     let ctx: CanvasRenderingContext2d = canvas.get_context().unwrap();
     let snake = Rc::new(RefCell::new(Snake::new(64, 48)));
+
+    // drawing
+    async_render_loop(snake.clone(), ctx);
 
     // input
     let snake_input_clone = snake.clone();
@@ -51,17 +52,16 @@ fn main() {
                 snake_input_clone.borrow_mut().change_direction(change.unwrap());
             }
         }
+        else if e.key().as_str() == " " {
+            snake_input_clone.borrow_mut().restart();
+        }
     });
-
-    // TODO: is clone needed?
-    async_render_loop(snake.clone(), ctx);
 
     stdweb::event_loop();
 }
-// pub fn set_timeout<F: FnOnce() + 'static>(callback: F, timeout: u32)
 
 fn async_render_loop(snake: Rc<RefCell<Snake>>, ctx: CanvasRenderingContext2d) {
-    window().request_animation_frame(move |timestamp| {
+    set_timeout(move || {
         snake.borrow_mut().step();
 
         let board: Value = snake.borrow_mut().get_rgba().into();
@@ -75,5 +75,5 @@ fn async_render_loop(snake: Rc<RefCell<Snake>>, ctx: CanvasRenderingContext2d) {
         }
 
         async_render_loop(snake, ctx);
-    });
+    }, 45);
 }
